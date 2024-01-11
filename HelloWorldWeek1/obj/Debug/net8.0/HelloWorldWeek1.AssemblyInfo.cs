da507359fe2e03a2a23f71741d1826edb2149047
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-01-11 14:12:38 -0700

    Working on C# learning

--- HelloWorldWeek1/obj/Debug/net8.0/HelloWorldWeek1.AssemblyInfo.cs
+++ HelloWorldWeek1/obj/Debug/net8.0/HelloWorldWeek1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572929ec8ea4172b548dc3d73a55f415078f404a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b52022f77449c75a2a38c040ec05846afcd93ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

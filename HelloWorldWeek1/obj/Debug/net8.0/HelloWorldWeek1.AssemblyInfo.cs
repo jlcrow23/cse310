12530c2954b4360a83fe8003452c9c025eb46840
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-01-11 17:12:01 -0700

    Fixing errors in sln

--- HelloWorldWeek1/obj/Debug/net8.0/HelloWorldWeek1.AssemblyInfo.cs
+++ HelloWorldWeek1/obj/Debug/net8.0/HelloWorldWeek1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710454197a345b05edcdd2e8132efbee9ff7fe32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldWeek1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
